6435c4834e0fbab999ab9adcbee65c46d75d1979
LoaiSalem9 <loaiahmedsalem9@gmail.com>
2024-03-19 12:34:46 +0200

    hello world

---
+++ src/Peers/Coordinator.java
@@ -0,0 +1,15 @@
+package Peers;
+
+import java.util.*;
+public class Coordinator {
+    LinkedList<Peer> peers;
+    public Coordinator(){
+        peers = new LinkedList<Peer>();
+    }
+    public void registerPeer(int port, String host, int targetPort){
+
+    }
+    public void unregisterPeer(int port){
+
+    }
+}
